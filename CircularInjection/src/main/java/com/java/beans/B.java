28afe7bc7a485003048260db25d2d4e60412820d
hpadmin <hpadmin@WIN-45DN5F07T0F>
2022-04-29 15:49:23 +0530

    First commit

---
+++ CircularInjection/src/main/java/com/java/beans/B.java
@@ -0,0 +1,20 @@
+package com.java.beans;
+
+public class B {
+
+	private A a;
+	
+	public B(A a) {
+		this.a = a;
+		System.out.println("B:1 param constructor");
+	}
+	public void setA(A a) {
+		System.out.println("B.setA()");
+		this.a = a;
+	}
+	@Override
+	public String toString() {
+		return "B [a=";
+	}
+	
+}
